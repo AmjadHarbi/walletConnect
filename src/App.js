0f7f234d2d35ee843a88631f8294477fcd362b52
AmjadHarbi <91230174+AmjadHarbi@users.noreply.github.com>
2023-02-07 13:04:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ import { useWeb3React } from '@web3-react/core'
 
 
 const CoinbaseWallet = new WalletLinkConnector({
-  url: https://mainnet.infura.io/v3/${process.env.INFURA_KEY},
+  url: 'https://mainnet.infura.io/v3/${process.env.INFURA_KEY}',
   appName: "Web3-react Demo",
   supportedChainIds: [1, 3, 4, 5, 42],
  });
  
  const WalletConnect = new WalletConnectConnector({
-  rpcUrl: https://mainnet.infura.io/v3/${process.env.INFURA_KEY},
+  rpcUrl: 'https://mainnet.infura.io/v3/${process.env.INFURA_KEY}',
   bridge: "https://bridge.walletconnect.org/",
   qrcode: true,
  });
